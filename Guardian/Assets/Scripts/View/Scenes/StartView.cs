5d4c51cdea592c6f5ee7eb5a7cd738d883cdc1d1
hexinping <hexinping456@126.com>
2019-04-04 09:13:05 +0800

    添加view 和 ctrl脚本

---
+++ Guardian/Assets/Scripts/View/Scenes/StartView.cs
@@ -0,0 +1,45 @@
+﻿
+/***
+ *
+ *  Title: "Guardian" 项目
+ *         描述：
+ *
+ *  Description:
+ *        功能：
+ *       
+ *
+ *  Date: 2019
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartView : MonoBehaviour {
+
+
+    private StartView_Ctrl _ctrl;
+
+    void Awake()
+    {
+        _ctrl = new StartView_Ctrl();
+    }
+
+    public void onClickNewBtn()
+    {
+        Debug.Log("click the NewBtn :"+ this.GetType());
+        _ctrl.onClickNewBtn();
+    }
+
+    public void onClickContinueBtn()
+    {
+        Debug.Log("click the ContinueBtn :" + this.GetType());
+        _ctrl.onClickContinueBtn();
+    }
+}
