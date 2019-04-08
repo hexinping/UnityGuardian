c880f4d31577953037793d0aca910e277c1de61e
hexinping <hexinping456@126.com>
2019-04-08 10:54:40 +0800

    viewManger第一版

---
+++ Guardian/Assets/Scripts/View/BaseView.cs
@@ -0,0 +1,99 @@
+﻿
+/***
+ *
+ *  Title: "Guardian" 项目
+ *         描述：
+ *
+ *  Description:
+ *        功能：
+ *       
+ *
+ *  Date: 2019
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseView : MonoBehaviour
+{
+
+    private string _name;            
+    private string _prefabName;
+
+    private GameObject _prefab;
+
+    //view的根节点
+    private GameObject _rootViewGameObject;
+
+    //dialog的根节点，dialog是基于view的
+    private GameObject _rootDialogGameObject;
+
+    private ViewManager _viewManager;
+
+    //public BaseView()
+    //{ 
+    
+    //}
+
+    public void Awake()
+    {
+        //_rootViewGameObject = new GameObject("rootViewNode");
+        //_rootDialogGameObject = new GameObject("rootDialogNode");
+        ////默认取屏幕中点
+        //Vector3 displayPos = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+        //setPos(displayPos);
+
+        //_viewManager = ViewManager.getInstance();
+
+    }
+
+    //public void Start()
+    //{
+    //    initUI();
+    //}
+
+    public void initUI(GameObject prefab, string name)
+    {
+        _rootViewGameObject = new GameObject("rootViewNode");
+        _rootDialogGameObject = new GameObject("rootDialogNode");
+        //默认取屏幕中点
+        Vector3 displayPos = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+        setPos(displayPos);
+
+        _viewManager = ViewManager.getInstance();
+
+        _name = name;
+        _prefab = prefab;
+        GameObject view = Instantiate(_prefab);
+        view.transform.parent = _rootViewGameObject.transform;
+
+        //页面dailog的层级要高于View
+        _rootDialogGameObject.transform.parent = _rootViewGameObject.transform;
+
+    }
+
+    public void setPos(Vector3 displayPos)
+    {
+        _rootViewGameObject.transform.localPosition = displayPos;
+    }
+
+    public GameObject getViewRoot()
+    {
+        return _rootViewGameObject;
+    }
+
+    public string getViewName()
+    {
+        return _name;
+    }
+
+    
+
+}
---
+++ Guardian/Assets/Scripts/View/ViewManager.cs
@@ -0,0 +1,66 @@
+﻿
+/***
+ *
+ *  Title: "Guardian" 项目
+ *         描述：
+ *
+ *  Description:
+ *        功能：
+ *       
+ *
+ *  Date: 2019
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewManager
+{
+
+    public Dictionary<string, BaseView> _viewDict = null;
+
+    private static ViewManager _instance = null;
+
+    public GameObject _viewLayer;
+
+
+    public ViewManager()
+    {
+        _viewDict = new Dictionary<string, BaseView>();
+    }
+
+    static public ViewManager getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ViewManager();
+        }
+        return _instance;
+    }
+
+    public void showView(GameObject prefab, string viewName)
+    {
+        BaseView view = new BaseView();
+        view.initUI(prefab, viewName);
+        GameObject viewRootNode = view.getViewRoot();
+        viewRootNode.transform.parent = _viewLayer.transform;
+
+        //页面管理
+        _viewDict[viewName] = view;
+
+    }
+
+    public void popView()
+    { 
+    
+    }
+
+
+}
